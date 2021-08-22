12ed773081132c68847e485dccc79f47bf3625ca
Muditha0829 <muditha1gms@gmail.com>
2021-08-22 20:27:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.cameraapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mimageView;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mimageView = findViewById(R.id.imageView);
+    }
+
+    public void takePicture(View view){
+        Intent imageCapture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(imageCapture.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(imageCapture,REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mimageView.setImageBitmap(imageBitmap);
+        }
+    }
+}}
